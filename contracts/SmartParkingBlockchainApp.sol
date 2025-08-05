d9b34edf0346f77fa589776b8b4aa6c5f83f4ff5
wdhenarangoda <warunahenarangoda@gmail.com>
2025-08-05 20:29:36 +1000

    Create SmartParkingBlockchainApp.sol

---
+++ contracts/SmartParkingBlockchainApp.sol
@@ -0,0 +1,9 @@
+// Student Name: Waruna Deshan Henarangoda
+// Student ID: 24910413
+// Student Email: w.henarangoda.10@student.scu.edu.au
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract SmartParkingBlockchainApp {
+    
+}
