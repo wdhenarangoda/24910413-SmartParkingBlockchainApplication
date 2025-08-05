b84c3630c46c91b5567b3b1d830d844b033eff66
wdhenarangoda <warunahenarangoda@gmail.com>
2025-08-05 20:34:30 +1000

    Add basic entity structs - Driver, SensorDevice, ServicePoint

--- contracts/SmartParkingBlockchainApp.sol
+++ contracts/SmartParkingBlockchainApp.sol
@@ -5,5 +5,29 @@
 pragma solidity ^0.8.20;
 
 contract SmartParkingBlockchainApp {
-    
+    // Driver Profile
+    struct Driver {
+        string name; // Driver name
+        string homeAddress; // Driver address
+        string licenseId; // Driver license or ID
+        bool exists; // Existence flag
+    }
+
+    // Sensor Device
+    struct SensorDevice {
+        bytes32 deviceId; // Unique device ID
+        string location; // Physical location
+        address owner; // Registrar address
+        bool exists; // Existence flag
+    }
+
+    // Service Point
+    struct ServicePoint {
+        bytes32 pointId; // Unique point ID
+        string name; // Point name
+        string location; // Physical location
+        address owner; // Registrar address
+        bool exists; // Existence flag
+    }
 }
+
